b90acd880bd8fedc5004dfba71c51aace1fe42af
mona-prajapati15 <monaprajapati696@gmail.com>
2026-05-08 18:57:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 import streamlit as st
-st.title("Fraud Detection Project")
+import streamlit as st
+
+st.set_page_config(page_title="Fraud Detection", layout="centered")
+
+st.title("💳 Online Payment Fraud Detection")
+
+st.write("Enter transaction details below")
+
+amount = st.number_input("Transaction Amount")
+oldbalanceOrg = st.number_input("Old Balance")
+newbalanceOrig = st.number_input("New Balance")
+
+type = st.selectbox(
+    "Transaction Type",
+    ["PAYMENT", "TRANSFER", "CASH_OUT", "DEBIT"]
+)
+
+if st.button("Detect Fraud"):
+
+    if amount > 100000:
+        st.error("🚨 Fraudulent Transaction Detected")
+    else:
+        st.success("✅ Safe Transaction")
 import joblib
 import numpy as np
